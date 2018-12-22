0fba90381129953283af2afe696450d28ba8ee23
Nico Gruschke <14308103+AmarokSama@users.noreply.github.com>
2018-12-22 23:48:30 +0100

    checkstyle fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,13 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * Greet function
+   *
+   * @param someone person to greed
+   * @return greedings
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
